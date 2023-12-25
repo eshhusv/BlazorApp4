e2c9126d988e6d1fa619bbbdbebdecfe888ca4a4
eshhus <ak7527203@gmail.com>
2023-12-25 15:10:24 +0200

    Добавьте файлы проекта.

---
+++ Models/CustomerService.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp4.Models
+{
+    public class CustomerService
+    {
+        public int CustomerId { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
---
+++ Models/TaskSercive.cs
@@ -0,0 +1,12 @@
+﻿using BlazorApp4.Components.Pages;
+
+namespace BlazorApp4.Models
+{
+    public class TaskSercive
+    {
+        public int TaskId { get; set; }
+
+        public string? TaskName { get; set; }
+
+    }
+}
